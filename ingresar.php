e91abac39eb389d5640c602c328f36e2fe2916c5
Rogers-437-61 <57192216+Rogers-437-61@users.noreply.github.com>
2020-07-03 19:25:44 -0300

    Add files via upload

---
+++ ingresar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main2.css" />
+    <meta name="keywords" content="HTML, CSS, Javascript, PHP, SQL" />
+    <meta name="description" content="Una vez creada la cuenta... debes iniciar para ver nuestro contenido." />
+    <title>Iniciar sesión</title>
+  </head>
+  <body>
+
+    <form class="" action="index.html" method="post">
+
+    </form>
+    
+    <div id="footer">
+			<section class="container">
+				<hr />
+			</section>
+			<!-- Copyright -->
+				<div id="copyright">
+					Brian Rogers. <strong>Todos los derechos reservados.</strong>
+				</div>
+		</div>
+  </body>
+</html>
